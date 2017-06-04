2a588af101b6bc4cf21e20e1c0f648403ed3a4f6
Sqeaky <sqeaky@blacktoppstudios.com>
2017-06-03 20:29:52 -0500

    Trim \r from cached values

--- Test/CMakeCache.rb
+++ Test/CMakeCache.rb
@@ -47,7 +47,7 @@ class CMakeCache
             if found = line.match(/^([^#]+):([^#]+)=([^#]+)$/)
                 name, type, value = found.captures
             end
-            @value_cache[name] = value
+            @value_cache[name] = value.tr("\r", '')
             @type_cache[name] = type
         end
     end
