66e13621aca96478968aaae79a31a97e7a329ce9
MakoEnergy <MakoEnergy02@gmail.com>
2019-01-13 20:25:18 -0600

    Updated to C++17.

--- Jagati.cmake
+++ Jagati.cmake
@@ -855,7 +855,7 @@ macro(SetCommonCompilerFlags)
     if(CompilerDesignNix)
 
         # These warnings work will work on all nix style compilers. Here are the most important flags:
-        # -std=c++14 - Set the C++ standard to C++14, might update all the Jagati Packages to 14 soon.
+        # -std=c++17 - Set the C++ standard to C++17.
         # -fno-strict-aliasing - Required for linking some of the Mezzanine dependencies correctly.
         # -Wall - Enables "all" compiler warnings, actually abour 2/3rds, including common stuff like bad inits.
         # -Wextra - Enable the rest of the warnings except some sketchy ones.
@@ -878,7 +878,7 @@ macro(SetCommonCompilerFlags)
         # -Wstrict-overflow=2 - When the compiler re-arranges some math that might cause an integer overflow.
         # -Wundef - Fail when undeclared preprocessor macros are used, almost always a bug/platform error.
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} \
-        -std=c++14 -Wall -Wextra -Werror -pedantic-errors \
+        -std=c++17 -Wall -Wextra -Werror -pedantic-errors \
         -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization -Wformat=2 -Wmissing-declarations \
         -Wmissing-include-dirs -Wold-style-cast -Wredundant-decls -Wshadow -Wconversion -Wsign-promo \
         -Wstrict-overflow=2 -Wundef")
